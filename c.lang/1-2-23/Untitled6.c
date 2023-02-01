0833f4b03a0dd4b106cbabbf9f7affa18bbba5af
ashishsonewane <124118703+ashishsonewane@users.noreply.github.com>
2023-02-01 16:25:45 +0530

    KBC.code

---
+++ c.lang/1-2-23/Untitled6.c
@@ -0,0 +1,363 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,ans;
+    
+	printf("   welcome to the game of KBC\n\n:    ");
+	printf("are you ready for the game: \n");
+	printf("Q1. Who is the father of C language?\n");
+    printf("1) Steve Jobs\n");
+    printf("2) James Gosling\n");
+    printf("3) Dennis Ritchie\n");
+    printf("4) Rasmus Lerdorf\n");
+    printf("5) quite the game\n");
+    printf("Answer=: ");
+    scanf("%d",&ans);
+    a=3;
+    b=5;
+    if(a==ans)
+    {
+    	printf("you are ans is correct\n");
+    	printf("you have win the 1000 rs\n\n");
+    	
+    	printf("2. Which of the following is not a valid C variable name?\n");
+        printf("1) int number\n");
+        printf("2) float rate\n");
+        printf("3) int variable_count\n");
+        printf("4) int $main\n");
+        printf("5) quite the game\n");
+        printf("Answer=: ");
+        scanf("%d",&ans);
+        a=4;
+        b=5;
+        if(a==ans)
+        {
+        	printf("you are ans is correct\n");
+        	printf("you have win the 2000rs\n\n");
+        	
+        	printf("Q3. All keywords in C are in ____________\n");
+            printf("1) LowerCase letters\n");
+            printf("2) UpperCase letters\n");
+            printf("3) CamelCase letters\n");
+            printf("4) None of the mentioned\n");
+            printf("5) quite the game\n");
+            printf("Answer=: ");
+            scanf("%d",&ans);
+            a=1;
+            b=5;
+            if(a==ans)
+            {
+            	printf("you are the ans is correct\n");
+            	printf("you have win the 3000\n\n");
+            	
+            	printf("Q4. Which of the following is true for variable names in C?\n");
+                printf("1) They can contain alphanumeric characters as well as special characters\n");
+                printf("2) It is not an error to declare a variable to be one of the keywords(like goto, static)\n");
+                printf("3) Variable names cannot start with a digit\n");
+                printf("4) Variable can be of any length\n");
+            	printf("5) quite the game\n");
+                printf("Answer=: ");
+                scanf("%d",&ans);
+                a=3;
+                b=5;
+                if(ans==a)
+                {
+                	printf("you are the ans is correct\n");
+                	printf("you have win the 5000\n");
+                	//Q5
+                	printf("Q5. Which is valid C expression?\n");
+                    printf("1) int my_num = 100,000\n");
+                    printf("2) int my_num = 100000\n");
+                    printf("3) int my num = 1000;\n");
+                    printf("4) int $my_num = 10000\n;");
+                    printf("5) quite the game\n");
+                    printf("Answer=: ");
+                    scanf("%d",&ans);
+                    a=2;
+                    b=5;
+                    if(a==ans)
+                    {
+                    printf("you are the ans is correct\n");
+                	printf("you have win the 10000\n\n");
+					
+					printf("Q6. Which of the following cannot be a variable name in C?\n");
+                    printf("1) volatile\n");
+                    printf("2) true\n");
+                    printf("3) friend\n");
+                    printf("4) export\n");
+					printf("5) quite the game\n");
+                    printf("Answer=: ");
+                    scanf("%d",&ans);
+                    a=1;
+                    b=5;
+                    if(a==ans)
+					{
+					printf("you are the ans is correct\n");
+                	printf("you have win the 20000\n\n");
+					
+					printf("Q7. What is short int in C programming?\n");
+                    printf("1) The basic data type of C\n");
+                    printf("2) Qualifier\n");
+                    printf("3) Short is the qualifier and int is the basic data type\n");
+                    printf("4) All of the mentioned\n");
+					printf("5) quite the game\n");
+                    printf("Answer=: ");
+                    scanf("%d",&ans);
+                    a=3;
+                    b=5;
+                    if(a==ans)
+                    {
+                 	printf("you are the ans is correct\n");
+                	printf("you have win the 40000\n\n");
+                	
+                	printf("Q8. Which of the following declaration is not supported by C language?\n");
+					printf("1) String str\n");
+				    printf("2) char *str\n");
+					printf("3) float str = 3e2\n");
+					printf("4) Both “String str;” and “float str = 3e2\n");
+                	printf("5) quite the game\n");
+                    printf("Answer=: ");
+                    scanf("%d",&ans);
+                    a=1;
+                    b=5;
+                    if(a==ans)
+                    {
+                    printf("you are the ans is correct\n");
+                	printf("you have win the 80000\n\n");
+					
+					//Q9
+					
+					printf("Q9. Which keyword is used to prevent any changes in the variable within a C program?\n");
+					printf("1) immutable\n");
+					printf("2) mutable\n");
+					printf("3) const\n");
+					printf("4) volatile\n");
+					printf("5) quite the game\n");
+                    printf("Answer=: ");
+                    scanf("%d",&ans);
+                    a=3;
+                    b=5;
+					if(a==ans)
+					{
+						 printf("you are the ans is correct\n");
+                	    printf("you have win the 160000\n\n");
+                	    
+                	    printf("Q10. What is the result of logical or relational expression in C?\n");
+					    printf("1) True or False\n");
+					    printf("2) 0 or 1\n");
+						printf("3) 0 if an expression is false and any positive number if an expression is true\n");
+					    printf("4) None of the mentioned\n");
+					    printf("5) quite the game\n");
+                        printf("Answer=: ");
+                    	scanf("%d",&ans);
+                    	a=2;
+                    	b=5;
+                    	if(a==ans)
+                    	{
+                    		printf("you are the ans is correct\n");
+                	   		 printf("you have win the 320000\n\n");	
+                	   		 
+                	   		 printf("Q11. Which of the following typecasting is accepted by C language?\n");
+							printf("1) Widening conversions\n");
+   							printf("2) Narrowing conversions\n");
+						    printf("3) Widening & Narrowing conversions\n");
+   							printf("4) None of the mentioned\n");
+                	   		printf("5) quite the game\n");
+                        	printf("Answer=: ");
+                    		scanf("%d",&ans);
+                    		a=3;
+                    		b=5;
+       						if(a==ans){
+       							printf("you are the ans is correct\n");
+                	   		    printf("you have win the 640000\n\n");
+                	   		    
+                	   		     printf("Q12. Where in C the order of precedence of operators do not exist?\n");
+          						printf("1) Within conditional statements, if, else\n");
+								printf("2) Within while, do-while\n");
+        						printf("3) Within a macro definition\n");
+                                printf("4) None of the mentioned\n");
+                	   		   	printf("5) quite the game\n");
+                        		printf("Answer=: ");
+                    			scanf("%d",&ans);
+                    			a=4;
+                    			b=5;
+                    			if(a==ans)
+                    			{
+                    			printf("you are the ans is correct\n");
+                	   		    printf("you have win the 1250000\n\n");
+                	   		    //Q13
+                	   		    printf("Q13. Which of the following is NOT possible with any 2 operators in C?\n");
+							    printf("1) Different precedence, same associativity\n");
+								printf("2) Different precedence, different associativity\n");
+								printf("3) Same precedence, different associativity\n");
+								printf("4) All of the mentioned\n");
+                	   		    printf("5) quite the game\n");
+                        		printf("Answer=: ");
+                    			scanf("%d",&ans);
+                    			a=3;
+                    			b=5;
+                    			if(a==ans)
+                    			{
+                    			printf("you are the ans is correct\n");
+                	   		    printf("you have win the 2500000\n\n");
+								   
+								//Q14
+								
+								
+									
+								}//Q13
+								else if(b==ans)
+								{
+								printf("you have quit the game\n");
+			     				printf("you have win the 1250000\n");	
+								}
+								else
+								{
+								printf("wrong the ans you are loss the game");
+								printf("you have win the 320000");	
+								}
+								}//Q12
+								else if(b==ans)
+								{
+									printf("you have quit the game\n");
+			     					printf("you have win the 640000\n");
+								}
+								else
+								{
+								printf("wrong the ans you are loss the game");
+								printf("you have win the 320000");	
+								}
+							   }//Q11
+							   else if(b==ans)
+							   {
+							   		printf("you have quit the game\n");
+			     					printf("you have win the 320000\n");
+							   }
+							   else
+							   {
+							   		printf("wrong the ans you are loss the game");
+									printf("you have win the 320000");	
+						}
+							   
+						}//Q10
+						else if(b==ans)
+						{
+						printf("you have quit the game\n");
+			     		printf("you have win the 160000\n");	
+						}
+						else
+						{
+						printf("wrong the ans you are loss the game");
+							printf("you have win the 10000");	
+						}
+						} //Q9
+						else if(b==ans)
+						{
+							printf("you have quit the game\n");
+			     			printf("you have win the 80000\n");	
+			     			
+			     			
+							}
+							else
+							{
+							printf("wrong the ans you are loss the game");
+							printf("you have win the 10000");	
+								}	
+					}//8
+                	else if(b==ans)
+                	{
+                	printf("you have quit the game\n");
+			     	printf("you have win the 40000");	
+					}
+					else
+					{
+						printf("wrong the ans you are loss the game");
+						printf("you have win the 10000");
+					}
+					}
+					//Q7
+					else if(b==ans)
+					{
+					printf("you have quit the game\n");
+			     	printf("you have win the 20000");	
+					}
+					else
+					{
+					printf("wrong the ans you are loss the game");
+					printf("you have win the 10000");	
+					}
+					}
+					
+					//Q6
+					else if(b==ans)
+					{
+					printf("you have quit the game\n");
+			     	printf("you have win the 10000");	
+					}
+					else
+					{
+					printf("wrong the ans you are loss the game");
+					printf("you have win the 10000");	
+					}	
+					} 
+					
+					///Q5
+					
+					else if(b==ans)
+					{
+					printf("you have quit the game\n");
+			     	printf("you have win the 5000");	
+					}
+					else
+					{ 
+					
+						printf("wrong the ans you are loss the game");
+					}
+                	
+				}//Q4
+				else if(b==ans)
+				{
+				printf("you have quit the game\n");
+				printf("you have win the 3000");	
+				}
+                else
+                {
+                	printf("wrong the ans you are loss the game");
+				}
+			}//Q3
+			else if(b==ans)
+			{
+				printf("you have quit the game\n");
+				printf("you have win the 2000");
+			}else
+			{
+			printf("wrong the ans you are loss the game");	
+			}
+		} 
+		//Q2
+		else if(b==ans)
+		{
+			printf("you have quit the game\n");
+			printf("you have win the 1000");
+		}
+		else
+		{
+			printf("wrong the ans you are loss the game");
+		}
+    	
+    //1Q	
+    }
+	else if(b==ans)
+	{
+			printf("you have quit the game\n");
+			printf("you have win the 0");
+	}
+	else
+	{
+		printf("wrong the ans you are loss the game");
+	}
+	
+
+	
+	return 0;
+}
